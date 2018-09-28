284a176304c8f96205f8dd5af3f1f90359da3172
Purnima <purnima@the-open.net>
2018-09-28 16:31:21 +0530

    Add and configure the Devise gem

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ end
 
 gem 'bootstrap-sass', '~> 3.3.6'
 gem 'jquery-rails'
+gem 'devise'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.6'
 # Use postgresql as the database for Active Record
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#index' # hash mean method/action in ruby. So this line says call PagesController and its 
                          #public method index.
