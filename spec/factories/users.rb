81aeccb0b47dbb15b693df6a1823ab53e3046958
Purnima <purnima@the-open.net>
2018-10-16 14:02:36 +0530

    Add a users factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,8 @@
-Factory.define do
-	factory :user do
-		sequence(:name) {|n| "test#{n}"} # do |n| "test#{n}"
-		sequence(:email) {|n| "test#{n}@test.com"}
-		password '123456'
-		password_confirmation '123456'
-	end	
+FactoryGirl.define do 
+  factory :user do
+    sequence(:name) { |n| "test#{n}" }
+    sequence(:email) { |n| "test#{n}@test.com" }
+    password {'123456'}
+    password_confirmation {'123456'}
+  end
 end
