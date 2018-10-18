97abcb8562787ce1b07459e6f72d059aac9a134c
Purnima <purnima@the-open.net>
2018-10-18 13:50:23 +0530

    Add color before feature

--- spec/features/user/sign_up_spec.rb
+++ spec/features/user/sign_up_spec.rb
@@ -1,19 +1,18 @@
 require "rails_helper"
+require "rails_helper"
 
-RSpec.feature "Sign Up", :type => feature do
-	# Devise’s build method instead of create. This way we 
-	# create a new object without saving it to the database.
-	let(:user) {build(:user)}
+RSpec.feature "Sign up", :type => :feature do
+  let(:user) { build(:user) }
 
-	scenario 'user navigates to sign up page and successfully signs up', js:true do
-		visit root_path
-		find('nav a', text: 'Signup').click
-		fill_in 'user[name]', with: user.name
-		fill_in 'user[email]', with: user.email
-		fill_in 'user[password]', with: user.password
-		fill_in 'user[password_confirmation]', with: user.password_confirmation
-		find('.sign-up-button').click
-		expect(page).to have_selector('#user-settings')
-	end
+  scenario 'user navigates to sign up page and successfully signs up', js: true do
+    visit root_path
+    find('nav a', text: 'Signup').click
+    fill_in 'user[name]', with: user.name
+    fill_in 'user[email]', with: user.email
+    fill_in 'user[password]', with: user.password
+    fill_in 'user[password_confirmation]', with: user.password_confirmation
+    find('.sign-up-button').click
+    expect(page).to have_selector('#user-settings')
+  end
+
 end
-
